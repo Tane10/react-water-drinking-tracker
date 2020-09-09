cb54ce28a93605e776a7952fcaedb9566a4aba2a
Tane10 <16818483+Tane10@users.noreply.github.com>
2020-09-09 12:47:53 +0100

    modal now has close button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import './App.css'
-import { Grid, makeStyles, Button, Container, Typography, Modal, TextField } from '@material-ui/core';
+import { Grid, makeStyles, Button, Typography,  } from '@material-ui/core';
 import { backGroundColor } from "./colors.js";
 import HollowMan from "./images/man_svg.svg";
 import ImageMask from "./images/IMG_MAN_MASK.svg";
-import Jeff from "./images/jeff.png";
-import Center from "react-center";
 import { Create } from '@material-ui/icons';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
 import UpdateWaterVoumeBtn from "./components/updateWaterVoumeBtn";
-// import EditWaterGoalModal from "./components/model/EditWaterGoalModal";
-import Carousel from 'react-elastic-carousel';
-import Item from './item';
-import "slick-carousel/slick/slick.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick-theme.css";
+import EditWaterGoalModal from "./components/modal/EditWaterGoalModal";
 
 
 const waterVolmueAmounts = [
@@ -87,40 +78,54 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-function App() {
 
+
+export default function App() {
   const classes = useStyles();
+
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   let totalWaterDrunk = 2.5;
   let achivedGoal = 15;
   let waterGoal = 3.5;
+
   return (
     <div style={{ backgroundColor: backGroundColor }}>
       <Grid direction="row" justify="center" alignItems="center" container className={classes.root}>
 
         <Grid direction="row" justify="center" alignItems="center" container spacing={5} style={{ paddingTop: '51px', paddingBottom: '33Px' }}>
-          <Grid item direction="column" xs={5} sm={6} md={2} container spacing={2}>
+          <Grid item direction="column" xs={5} sm={6} md={5} container spacing={2}>
             <Typography align="center" fontWeight="fontWeightBold" className={classes.typographyStyleNumValues} >{totalWaterDrunk}L
               </Typography>
             <Typography align="center" className={classes.typographyStyle} >TOTAL WATER DRUNK</Typography>
           </Grid>
 
-          <Grid item direction="column" xs={5} sm={6} md={2} container spacing={2}>
+          <Grid item direction="column" xs={5} sm={6} md={5} container spacing={2}>
             <Typography align="center" className={classes.typographyStyleNumValues} >{achivedGoal}</Typography>
             <Typography align="center" className={classes.typographyStyle} >ACHIEVED GOAL
                 DAYS</Typography>
           </Grid>
         </Grid>
 
-        <Grid>
-          <Button>
-            <Typography fontWeight="fontWeightBold" className={classes.typographyStyleBold} >{waterGoal}L
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={6}  sm={4} md={1} >
+            <Button onClick={handleOpen}>
+              <Typography fontWeight="fontWeightBold" className={classes.typographyStyleBold} >{waterGoal}L
           <Create style={{ fontSize: 15 }} />
-            </Typography>
-          </Button>
-          <br></br>
+              </Typography>
+            </Button>
 
 
-          {/* <div style={{
+            {/* <div style={{
             backgroundColor: "red",
             height: '200px',
             width: '100px',
@@ -129,24 +134,36 @@ function App() {
             // maskSize: '30px'
 
           }} > test */}
-          <img src={HollowMan} alt="HollowMan" height=""
-            style={{
-              WebkitMaskImage: `url(${ImageMask})`,
-              maskImage: `url(${ImageMask})`,
-              backgroundColor: "#4E96D3",
-              maskSize: 'auto',
-              WebkitMaskSize: 'auto',
-              maskRepeat: "no-repeat",
-              WebkitMaskRepeat: "no-repeat"
-            }} />
+            <img src={HollowMan} alt="HollowMan" height=""
+              style={{
+                WebkitMaskImage: `url(${ImageMask})`,
+                maskImage: `url(${ImageMask})`,
+                backgroundColor: "#4E96D3",
+                maskSize: 'auto',
+                WebkitMaskSize: 'auto',
+                maskRepeat: "no-repeat",
+                WebkitMaskRepeat: "no-repeat"
+              }} />
+          </Grid>
 
-          <Typography fontWeight="fontWeightBold" align="center" className={classes.typographyStyleBold} > Nice work! Keep it up!</Typography>
-          <Grid container  style={{paddingBottom: '24px', paddingTop:'20px'}} >
-              {waterVolmueAmounts.map(item => <Button style={{paddingLeft: '6px', paddingRight:'6px',  color: "white",
-    fontWeight: "bold",}}key={item.id}>{item.name} </Button> )}
+          <Grid container  justify="center" alignItems="center" >
+            <Typography fontWeight="fontWeightBold" align="center" className={classes.typographyStyleBold} > Nice work! Keep it up!</Typography>
+            <Grid container justify="center" alignItems="center" >
+              {waterVolmueAmounts.map(item => 
+              <Grid item xs={3}  sm={2} md={1} >
+                <Button style={{ color: "white",
+                paddingTop:'13px',
+                paddingBottom: '13px',
+                fontWeight: "bold",
+              }} key={item.id}>{item.name} </Button>
+                </Grid>)}
+            </Grid>
           </Grid>
 
           <UpdateWaterVoumeBtn />
+          <EditWaterGoalModal 
+          openModal={open}
+          closeModal={handleClose}/>
         </Grid>
       </Grid>
     </div>
@@ -155,7 +172,3 @@ function App() {
 
 
 
-
-
-
-export default App;
