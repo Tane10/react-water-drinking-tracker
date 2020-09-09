94ec18bb0723a7ee4b9f962aa706765fcc8c505f
Tane10 <16818483+Tane10@users.noreply.github.com>
2020-09-09 10:14:14 +0100

    can't scroll but we have buttons

--- src/App.js
+++ src/App.js
@@ -10,7 +10,20 @@ import { Create } from '@material-ui/icons';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import UpdateWaterVoumeBtn from "./components/updateWaterVoumeBtn";
-import EditWaterGoalModal from "./components/model/EditWaterGoalModal";
+// import EditWaterGoalModal from "./components/model/EditWaterGoalModal";
+import Carousel from 'react-elastic-carousel';
+import Item from './item';
+import "slick-carousel/slick/slick.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+
+
+const waterVolmueAmounts = [
+  { id: 1, name: '150 ml' },
+  { id: 2, name: '250 ml' },
+  { id: 3, name: '350 ml' },
+  { id: 4, name: '450 ml' },
+];
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -67,47 +80,33 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: '22px',
     fontSize: '14px'
   },
-  textField:{
+  textField: {
     paddingBottom: '13px',
     borderRadius: '20px'
   }
 }));
 
 
-function App()  {
+function App() {
 
   const classes = useStyles();
-
-
-
-
-  // const [open, setOpen] = React.useState(false);
-
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
-  // const [spacing, setSpacing] = React.useState(2);
   let totalWaterDrunk = 2.5;
   let achivedGoal = 15;
   let waterGoal = 3.5;
   return (
-    <div style={{ backgroundColor: backGroundColor}}>
+    <div style={{ backgroundColor: backGroundColor }}>
       <Grid direction="row" justify="center" alignItems="center" container className={classes.root}>
 
-        <Grid direction="row" justify="center" alignItems="center" container spacing={5} style={{paddingTop: '51px', paddingBottom: '33Px'}}>
+        <Grid direction="row" justify="center" alignItems="center" container spacing={5} style={{ paddingTop: '51px', paddingBottom: '33Px' }}>
           <Grid item direction="column" xs={5} sm={6} md={2} container spacing={2}>
-              <Typography align="center" fontWeight="fontWeightBold" className={classes.typographyStyleNumValues} >{totalWaterDrunk}L
+            <Typography align="center" fontWeight="fontWeightBold" className={classes.typographyStyleNumValues} >{totalWaterDrunk}L
               </Typography>
-              <Typography align="center" className={classes.typographyStyle} >TOTAL WATER DRUNK</Typography>
+            <Typography align="center" className={classes.typographyStyle} >TOTAL WATER DRUNK</Typography>
           </Grid>
 
-          <Grid item direction="column" xs={5} sm={6} md={2} container  spacing={2}>
-              <Typography align="center" className={classes.typographyStyleNumValues} >{achivedGoal}</Typography>
-              <Typography align="center" className={classes.typographyStyle} >ACHIEVED GOAL
+          <Grid item direction="column" xs={5} sm={6} md={2} container spacing={2}>
+            <Typography align="center" className={classes.typographyStyleNumValues} >{achivedGoal}</Typography>
+            <Typography align="center" className={classes.typographyStyle} >ACHIEVED GOAL
                 DAYS</Typography>
           </Grid>
         </Grid>
@@ -120,7 +119,7 @@ function App()  {
           </Button>
           <br></br>
 
-         
+
           {/* <div style={{
             backgroundColor: "red",
             height: '200px',
@@ -130,18 +129,23 @@ function App()  {
             // maskSize: '30px'
 
           }} > test */}
-          <img src={HollowMan} alt="HollowMan" height="" 
-          style={{
-          WebkitMaskImage: `url(${ImageMask})`,
-          maskImage: `url(${ImageMask})`, 
-          backgroundColor: "#4E96D3", 
-          maskSize: 'auto',
-          WebkitMaskSize: 'auto',
-          maskRepeat: "no-repeat",
-          WebkitMaskRepeat: "no-repeat"}}/>
+          <img src={HollowMan} alt="HollowMan" height=""
+            style={{
+              WebkitMaskImage: `url(${ImageMask})`,
+              maskImage: `url(${ImageMask})`,
+              backgroundColor: "#4E96D3",
+              maskSize: 'auto',
+              WebkitMaskSize: 'auto',
+              maskRepeat: "no-repeat",
+              WebkitMaskRepeat: "no-repeat"
+            }} />
 
           <Typography fontWeight="fontWeightBold" align="center" className={classes.typographyStyleBold} > Nice work! Keep it up!</Typography>
-          <Typography> val carosole</Typography>
+          <Grid container  style={{paddingBottom: '24px', paddingTop:'20px'}} >
+              {waterVolmueAmounts.map(item => <Button style={{paddingLeft: '6px', paddingRight:'6px',  color: "white",
+    fontWeight: "bold",}}key={item.id}>{item.name} </Button> )}
+          </Grid>
+
           <UpdateWaterVoumeBtn />
         </Grid>
       </Grid>
