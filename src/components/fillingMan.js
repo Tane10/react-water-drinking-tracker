5a911d663acc38c3b595ce523a2ca77a8a7edacb
Tane10 <16818483+Tane10@users.noreply.github.com>
2020-09-10 20:49:29 +0100

    finally got the man to fill upgit add *

--- src/components/fillingMan.js
+++ src/components/fillingMan.js
@@ -3,47 +3,21 @@ import HollowMan from "../images/man_svg.svg";
 import ImageMask from "../images/color_man_mask.svg";
 
 export const FillingMan = ({howFull}) => {
-
-    const postionref = React.useRef();
-    const [maskPostion, setMaskPostion] = React.useState();
-    const [maskHeight, setMaskheight] = React.useState();
-    const [dimensions, setDimensions] = React.useState({
-        height: window.innerHeight,
-        width: window.innerWidth
-    })
-
-    React.useEffect(() => {
-
-        setMaskPostion(postionref.current.offsetHeight);
-        console.log(postionref)
-
-        // function handleResize() {
-        //     setDimensions({
-        //         height: window.innerHeight,
-        //         width: window.innerWidth
-        //     });
-
-        //     window.addEventListener('resize', handleResize);
-
-        //     return _ => {
-        //         window.removeEventListener('resize', handleResize)
-
-        //     }
-        // }
-
-    }, [postionref, maskPostion, dimensions, maskHeight])
-
-    console.log(howFull)
-
     return (
-        <div>
-            <img ref={postionref} src={HollowMan} alt="HollowMan"
-                style={{ zIndex: '2', }} />
+        <div style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr',
+            gridTemplateRows: '1fr',
+            gridTemplateAreas: 'overlap'
+        }}>
+            <img  src={HollowMan} alt="HollowMan"
+                style={{ zIndex: '2', gridArea: 'overlap'}} />
             <img src={ImageMask} alt="HollowMan"
                 style={{
-                    zIndex: '1', position: "relative",
-                    bottom: `${maskPostion}px`, clipPath: `polygon(0 ${howFull}%, 100% ${howFull}%, 100% 100%, 0% 100%)`,
-                    height: `${maskHeight}px`
+                    zIndex: '1', 
+                     clipPath: `polygon(0 ${howFull}%, 100% ${howFull}%, 100% 100%, 0% 100%)`,
+                    height: "440px",
+                    gridArea: 'overlap',
                 }} />
         </div>
     )
